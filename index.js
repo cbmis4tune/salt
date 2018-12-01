d2d916758e5a6828872b1b65065538ddaa03a401
cbmis4tune <45508984+cbmis4tune@users.noreply.github.com>
2018-12-01 02:59:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ bot.registry.registerGroup('simple','Simple');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + '/commands');
 
+/*
 bot.on('message', function(message){
     if(message.content == 'add')
     {
         message.channel.sendMessage('Adding Salt!');
     }
 });
+*/
 
 bot.on('ready',function(){
     console.log("Ready for action Chris!");
