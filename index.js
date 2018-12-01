0d412b1d38fc25c12dd86ffd1bca62e50568df3d
cbmis4tune <45508984+cbmis4tune@users.noreply.github.com>
2018-12-01 02:09:07 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Commando = require('discord.js-commando');
+const bot = new Commando.Client();
+//const TOKEN =  'NTEyMzIxMDkwOTc5MTAyNzMx.DuPLyQ.-sKrXZynKpUdrrlfoFVqUnLfCQA';
+
+bot.registry.registerGroup('simple','Simple');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + '/commands');
+
+bot.on('message', function(message){
+    if(message.content == 'add')
+    {
+        message.channel.sendMessage('Adding Salt!');
+    }
+});
+
+bot.on('ready',function(){
+    console.log("Ready for action Chris!");
+})
+
+bot.login(process.env.BOT_TOKEN);
